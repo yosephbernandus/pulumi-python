aab94dd716a45c2627ba2e485090f15eedb8aafd
yosephbernandus <yosephbernandus@gmail.com>
2024-05-26 06:13:11 +0700

    testing

--- __main__.py
+++ __main__.py
@@ -1,11 +1,16 @@
 """A Kubernetes Python Pulumi program"""
 
 import pulumi
+import pulumi_kubernetes as k8s
 from pulumi_kubernetes.apps.v1 import Deployment, DeploymentSpecArgs
 from pulumi_kubernetes.meta.v1 import LabelSelectorArgs, ObjectMetaArgs
 from pulumi_kubernetes.core.v1 import ContainerArgs, PodSpecArgs, PodTemplateSpecArgs
 
 app_labels = { "app": "nginx" }
+
+config = pulumi.Config()
+kubeconfig = config.get("kubeconfig")
+provider = k8s.Provider("k8s-provider", kubeconfig=kubeconfig)
 
 deployment = Deployment(
     "nginx",
@@ -16,6 +21,8 @@ deployment = Deployment(
             metadata=ObjectMetaArgs(labels=app_labels),
             spec=PodSpecArgs(containers=[ContainerArgs(name="nginx", image="nginx")])
         ),
-    ))
+    ),
+    opts=pulumi.ResourceOptions(provider=provider)
+)
 
 pulumi.export("name", deployment.metadata["name"])
