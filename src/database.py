bab44e1d34e494020cba63a143dad1c44fa852dd
yosephbernandus <yosephbernandus@gmail.com>
2024-06-01 23:04:49 +0700

    postgres

--- src/database.py
+++ src/database.py
@@ -19,3 +19,20 @@ k8s.helm.v3.Chart(
     ),
     opts=pulumi.ResourceOptions(provider=cluster.provider),
 )
+
+
+k8s.core.v1.PersistentVolumeClaim(
+    resource_name="postgres-pvc",
+    metadata=k8s.meta.v1.ObjectMetaArgs(
+        name="postgres-pvc",
+        namespace="database",
+        annotations={"pulumi.com/skipAwait": "true"},
+    ),
+    spec=k8s.core.v1.PersistentVolumeClaimSpecArgs(
+        access_modes=["ReadWriteOnce"],
+        resources=k8s.core.v1.VolumeResourceRequirementsArgs(
+            requests={"storage": "8Gi"}
+        ),
+    ),
+    opts=pulumi.ResourceOptions(provider=cluster.provider),
+)
